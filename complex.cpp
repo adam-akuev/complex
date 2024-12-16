281a45c4a42be59afdd673de0cb45f5c914245a0
Adam Akuev <akuev2005@mail.ru>
2024-12-16 22:45:52 +0300

    Add static library for complex numbers

---
+++ complex.cpp
@@ -0,0 +1,18 @@
+﻿namespace MathLib {
+    struct Complex {
+        double real; // Действительная часть
+        double imag; // Мнимая часть
+    };
+
+    // Функция для сложения комплексных чисел
+    Complex add(const Complex& a, const Complex& b);
+
+    // Функция для вычитания комплексных чисел
+    Complex subtract(const Complex& a, const Complex& b);
+
+    // Функция для умножения комплексных чисел
+    Complex multiply(const Complex& a, const Complex& b);
+
+    // Функция для вычисления модуля комплексного числа
+    double modulus(const Complex& a);
+}
